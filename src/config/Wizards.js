10ebb53a50b0bf2b0ee6a2a7260c706bb4eda306
wcrbrm <viclopata@gmail.com>
2018-06-26 16:12:30 +0300

    more menus for wizards

--- src/config/Wizards.js
+++ src/config/Wizards.js
@@ -10,3 +10,27 @@ export const InstallationMenu = [
   '/confirm/pin',
   '/setup/complete'
 ];
+
+export const CreateMenu = network => [
+  '/create',
+  `/create/${network}/wallet`,
+  `/create/${network}/complete`
+];
+
+export const ImportMenu = network => [
+  '/import',
+  `/import/${network}/wallet`,
+  `/import/${network}/complete`
+];
+
+export const WatchMenu = network => [
+  '/watch',
+  `/watch/${network}/wallet`,
+  `/watch/${network}/complete`
+];
+
+export const ExchangeMenu = exchange => [
+  '/exchange',
+  `/exchange/${exchange}/account`,
+  `/exchange/${exchange}/complete`
+];
