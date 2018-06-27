bf42bb4f5e68de69f4dd27c56c577057c49619b3
wcrbrm <viclopata@gmail.com>
2018-06-27 12:12:47 +0300

    JDENTicon rendering component

---
+++ src/components/jdenticon/colorTheme.js
@@ -0,0 +1,25 @@
+﻿/**
+ * Jdenticon
+ * https://github.com/dmester/jdenticon
+ * Copyright © Daniel Mester Pirttijärvi
+ */
+
+import color from './color';
+
+/**
+ * Gets a set of identicon color candidates for a specified hue and config.
+ */
+const colorTheme = (hue, config) => [
+  // Dark gray
+  color.hsl(0, 0, config.grayscaleLightness(0)),
+  // Mid color
+  color.correctedHsl(hue, config.saturation, config.colorLightness(0.5)),
+  // Light gray
+  color.hsl(0, 0, config.grayscaleLightness(1)),
+  // Light color
+  color.correctedHsl(hue, config.saturation, config.colorLightness(1)),
+  // Dark color
+  color.correctedHsl(hue, config.saturation, config.colorLightness(0))
+];
+
+export default colorTheme;
---
+++ src/components/jdenticon/point.js
@@ -0,0 +1,12 @@
+﻿/**
+ * Jdenticon
+ * https://github.com/dmester/jdenticon
+ * Copyright © Daniel Mester Pirttijärvi
+ */
+
+/* eslint-disable */
+function Point(x, y) {
+  this.x = x;
+  this.y = y;
+};
+export default Point;
