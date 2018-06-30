8282f2b445db5e3335fdbc7079b29336cd61a954
wcrbrm <viclopata@gmail.com>
2018-06-30 19:20:58 +0300

    list of exchanges updated

--- src/config/Exchanges.js
+++ src/config/Exchanges.js
@@ -1,18 +1,26 @@
 export const Exchanges = [
-  { value: 'kucoin',   name: 'Kucoin',   icon: '/networks/BTC.png', keys: [
+  { value: 'binance',  name: 'Binance',  icon: '/exchanges/binance.png', keys: [
+    { id: "apiKey", label: "API KEY" },
+    { id: "secretApiKey", label: "Secret" }
+  ] },
+  { value: 'bitfinex', name: 'Bitfinex', icon: '/exchanges/bitfinex.png', keys: [
+    { id: "apiKey", label: "API KEY" },
+    { id: "secretApiKey", label: "Secret" }
+  ] },
+  { value: 'bittrex',  name: 'Bittrex',  icon: '/exchanges/bittrex.png', keys: [
+    { id: "apiKey", label: "API KEY" },
+    { id: "secretApiKey", label: "Secret" }
+  ] },
+  { value: 'hitbtc',   name: 'HitBTC',   icon: '/exchanges/hitbtc.png', keys: [
       { id: "apiKey", label: "API KEY" },
-      { id: "secretApiKey", label: "Secret API KEY" }
+      { id: "secretApiKey", label: "Secret" }
   ] },
-  { value: 'binance',  name: 'Binance',  icon: '/networks/BTC.png', keys: [
-    { id: "apiKey", label: "API KEY" },
-    { id: "secretApiKey", label: "Secret API KEY" }
+  { value: 'kucoin',   name: 'Kucoin',   icon: '/exchanges/kucoin.png', keys: [
+      { id: "apiKey", label: "API KEY" },
+      { id: "secretApiKey", label: "Secret" }
   ] },
-  { value: 'bittrex',  name: 'Bittrex',  icon: '/networks/BTC.png', keys: [
-    { id: "apiKey", label: "API KEY" },
-    { id: "secretApiKey", label: "Secret API KEY" }
-  ] },
-  { value: 'bitfinex', name: 'Bitfinex', icon: '/networks/BTC.png', keys: [
-    { id: "apiKey", label: "API KEY" },
-    { id: "secretApiKey", label: "Secret API KEY" }
+  { value: 'liqui',   name: 'Liqui',   icon: '/exchanges/liqui.png', keys: [
+      { id: "apiKey", label: "API KEY" },
+      { id: "secretApiKey", label: "Secret" }
   ] }
 ];
