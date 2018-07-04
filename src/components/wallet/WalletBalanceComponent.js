f227a5c662e0124c31d05675a389b4c2a6bdd22e
wcrbrm <viclopata@gmail.com>
2018-07-04 19:16:19 +0300

    wallet balance: send and receiv ebuttons

--- src/components/wallet/WalletBalanceComponent.js
+++ src/components/wallet/WalletBalanceComponent.js
@@ -1,24 +1,96 @@
 import React from 'react';
-import { WalletPanel } from './../panel/index';
-import { Totals } from './../panel/Totals';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { WalletPanel, Totals } from './../panel/index';
 
-export const WalletBalanceComponent = () => 
-{ 
+const Send = styled.button`
+  position: absolute;
+  left: 135px;
+  top: 65px;
+  height: 70px;
+  width: 70px;
+
+  cursor: pointer;
+  text-transform: uppercase;
+  font-size: 10px;
+  font-weight: bold;
+  color: #252525;
+
+  outline: none !important;
+
+  border-radius: 50%;
+  margin: 0px auto;
+  border-left: none;
+  border-right: none;
+  border-top: 1px solid #6137bf;
+  border-bottom: 1px solid #61c38b;
+  box-shadow: 0px 0px 30px #cebdf1;
+  background: transparent;
+
+  &:hover {
+    background: #efefef;
+    color: #6c56f0;
+  }
+`;
+
+
+const Receive = styled.button`
+  position: absolute;
+  left: -75px;
+  top: 64px;
+  height: 70px;
+  width: 70px;
+
+  cursor: pointer;
+  text-transform: uppercase;
+  font-size: 10px;
+  font-weight: bold;
+  color: #252525;
+
+  outline: none !important;
+
+  border-radius: 50%;
+  margin: 0px auto;
+  border-left: none;
+  border-right: none;
+  border-top: 1px solid #6137bf;
+  border-bottom: 1px solid #61c38b;
+  box-shadow: 0px 0px 30px #cebdf1;
+  background: transparent;
+
+  &:hover {
+    background: #efefef;
+    color: #6c56f0;
+  }
+`;
+
+
+const _t = {
+  receive: 'Receive',
+  send: 'Send'
+};
+
+export const WalletBalanceComponent = () =>
+{
   const address = '0xAA2303232020302302323030303030';
   const network = 'ETH';
   const name = 'My ETH Wallet';
   const icon = '/networks/ETH.png';
+  const walletUrl = suffix => (`/wallets/${address}/${suffix}`);
   return (
     <WalletPanel {...{ address, name, network, icon }}>
 
-      <Totals value={'1,144'} currency={'USD'} />
+      <Totals value={'1,144'} currency={'USD'}>
+        <Link to={walletUrl('send')}>
+          <Send>{_t.send}</Send>
+        </Link>
+        <Link to={walletUrl('receive')}>
+          <Receive>{_t.receive}</Receive>
+        </Link>
+      </Totals>
+
       <br />
       TODO: transactions
-      <br />
-      TODO: send button
-      <br />
-      TODO: receive button
-
       <br />
       <br />
       lorem ipsum
