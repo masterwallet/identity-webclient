6607e96210879ce8d165a87510ec62bc0c4c18a3
wcrbrm <viclopata@gmail.com>
2018-07-04 20:08:12 +0300

    mock of screen for receiving

--- src/components/panel/WalletPanel.js
+++ src/components/panel/WalletPanel.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 import { JDentIcon } from './../jdenticon/index';
 import { NetworkIcon } from './../assets/NetworkIcon';
 
@@ -24,7 +25,7 @@ const Wrapper = styled.div`
   @media(max-width: 480px) {
     .top { font-size: 16px; }
   }
-  
+
   .name {
     font-size: 18px;
     font-weight: bold;
@@ -48,7 +49,7 @@ const Wrapper = styled.div`
     text-shadow: #61c38b;
   }
 
-  .lc { 
+  .lc {
     position: fixed;
     left: 0px;
     top: 0px;
@@ -58,14 +59,73 @@ const Wrapper = styled.div`
 const Spacer = styled.div`
   height: 75px;
 `;
-  
+
 const shortAddress = (value) => (value.substring(0, 6) + " ... " + value.substring(value.length - 10));
-  
-export const WalletPanel = ({ name, address, network, icon, children }) => (
+
+
+const ChveronLeft = () => (
+  <svg viewBox="0 0 256 512"  style={{ width: 10, transform: "scale(-1,1)" }}>
+    <path fill="currentColor" d="M17.525 36.465l-7.071 7.07c-4.686 4.686-4.686 12.284 0 16.971L205.947 256 10.454 451.494c-4.686 4.686-4.686 12.284 0 16.971l7.071 7.07c4.686 4.686 12.284 4.686 16.97 0l211.051-211.05c4.686-4.686 4.686-12.284 0-16.971L34.495 36.465c-4.686-4.687-12.284-4.687-16.97 0z" />
+  </svg>
+);
+
+
+const ButtonWrapper = styled.div`
+  position: fixed;
+  z-index: 1001;
+  left: 5px;
+  top: 15px;
+
+  a.btn {
+    display: flex;
+    align-items: center;
+
+    background: transparent;
+    font-weight: bold;
+    color: #262327;
+    border-top: 1px #6239bf solid;
+    border-bottom: 1px #61c38b solid;
+
+    border-radius: 0px;
+  }
+  a.btn:hover {
+    opacity: 0.9;
+    background: #fff;
+    color: #6b4afa;
+    border-top: 1px #61c38b solid;
+    border-bottom: 1px #6239bf solid;
+  }
+
+  a .title { margin-left: 15px; }
+  @media(max-width: 480px) {
+    a .title {
+      display: none;
+    }
+  }
+`;
+
+export const BackButton = ({ title, to, disabled = false }) => (
+  <ButtonWrapper>
+    <Link disabled={disabled} to={to} className="btn btn-primary btn-sm">
+      <ChveronLeft />
+      <span className="title">{title}</span>
+    </Link>
+  </ButtonWrapper>
+);
+
+const _t = {
+  backToWallet: 'Back to Wallet'
+};
+
+export const WalletPanel = ({ name, address, network, icon, children, back = false }) => (
   <Wrapper>
     <div className="top">
-      <div className="lc">
-      </div>
+      <ButtonWrapper>
+        <Link to={`/wallets/${address}/balance`} className="btn btn-primary btn-sm">
+          <ChveronLeft />
+          <span className="title">{_t.backToWallet}</span>
+        </Link>
+      </ButtonWrapper>
       <div style={{ display: 'flex', alignItems: 'center', width: 300, margin: '0px auto' }}>
         <div style={{ width: 50 }}>
           <NetworkIcon network={network} icon={icon} />
@@ -76,7 +136,7 @@ export const WalletPanel = ({ name, address, network, icon, children }) => (
           <div className="address">{shortAddress(address)}</div>
         </div>
       </div>
-    </div>          
+    </div>
     <Spacer />
     <div style={{ width: 300, margin: '0px auto', textAlign: 'justify' }}>
       {children}
--- src/components/wallet/WalletReceiveComponent.js
+++ src/components/wallet/WalletReceiveComponent.js
@@ -1,7 +1,36 @@
 import React from 'react';
+// import { Link } from 'react-router-dom';
+// import styled from 'styled-components';
+import { WalletPanel } from './../panel/index';
+import { JDentIcon } from './../jdenticon/index';
+import QRCode from 'qrcode.react';
 
-export const WalletReceiveComponent = () => (
-  <div>
-    Wallet Receive
-  </div>
-);
+const _t = {
+  address: 'Public Address',
+  receive: 'Address To Send',
+  imageForVerification: 'Image For Verification'
+};
+
+export const WalletReceiveComponent = () => {
+  const address = '0xAA2303232020302302323030303030';
+  const network = 'ETH';
+  const name = 'My ETH Wallet';
+  const icon = '/networks/ETH.png';
+  // const walletUrl = suffix => (`/wallets/${address}/${suffix}`);
+  return (
+    <WalletPanel {...{address, name, network, icon}} back={true}>
+      <h3 style={{ fontSize: 18, textAlign: 'center', color: '#8760f6' }}>{_t.receive}</h3>
+      <div style={{ margin: '0px auto', width: 180 }}>
+        <QRCode value={address} style={{ width: 180, height: 180 }} />
+      </div>
+      <div style={{ textAlign: 'center', fontSize: 12, fontWeight: 'bold', margin: '20px 0px' }}>
+        {_t.address}:<br />
+        {address}
+      </div>
+      <div style={{ textAlign: 'center', fontSize: 12, color: '#888' }}>{_t.imageForVerification}</div>
+      <div style={{ margin: '0px auto', width: 100,  background: '#fff' }}>
+        <JDentIcon value={address} size={100} />
+      </div>
+    </WalletPanel>
+  );
+};
