cc9fe397dd951c4fabf1b577c440702c97b3fc57
wcrbrm <viclopata@gmail.com>
2018-07-07 20:41:17 +0300

    install complete component - cleanup

--- src/components/install/CompleteComponent.js
+++ src/components/install/CompleteComponent.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Steps } from './../controls/Steps';
-import { InstallationMenu } from './../../config/Wizards';
+import { InstallationMenu, findWizardStep } from './../../config/Wizards';
 import { WizardPanel, Next } from './../panel/index';
 
 const _t = {
@@ -10,17 +10,21 @@ const _t = {
   nowYouCan: 'Now you can easily add wallets and manage them',
   start: 'Start'
 };
-export const CompleteComponent = () => (
-  <WizardPanel title={_t.isComplete} wide={false}>
-    <Next title={_t.start} to={'/add'} />
+export const CompleteComponent = () => {
+  const menu = InstallationMenu;
+  const step = findWizardStep(menu, '/complete');
+  return (
+    <WizardPanel title={_t.isComplete} wide={false}>
+      <Next title={_t.start} to={'/add'} />
 
-    <div style={{ margin: '20px auto', textAlign: 'center' }}>
-      <img src='/media/applause.png' alt='' style={{ width: '70%', height: 'auto' }} />
-      <h4 style={{ textAlign: 'center'}}>{_t.congrats}</h4>
-      <p style={{ textAlign: 'center', marginBottom: 0, fontWeight: 'bold' }}>{_t.successfullyFinished}</p>
-    </div>
-    <p style={{ textAlign: 'center' }}>{_t.nowYouCan}</p>
+      <div style={{ margin: '20px auto', textAlign: 'center' }}>
+        <img src='/media/applause.png' alt='' style={{ width: 210, height: 210 }} />
+        <h4 style={{ textAlign: 'center'}}>{_t.congrats}</h4>
+        <p style={{ textAlign: 'center', marginBottom: 0, fontWeight: 'bold' }}>{_t.successfullyFinished}</p>
+      </div>
+      <p style={{ textAlign: 'center' }}>{_t.nowYouCan}</p>
 
-    <Steps {...{ step: 9, menu: InstallationMenu }} />
-  </WizardPanel>
-);
+      <Steps {...{ step, menu }} />
+    </WizardPanel>
+  );
+};
