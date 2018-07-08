b4a724e9391442bedcf099e104d2c674af36d7ab
wcrbrm <viclopata@gmail.com>
2018-07-08 17:44:41 +0300

    welcome component cleanup

--- src/components/install/WelcomeComponent.js
+++ src/components/install/WelcomeComponent.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import { Steps } from './../controls/Steps';
 import { InstallationMenu } from './../../config/Wizards';
-import { FromFile } from './../../locale/index';
 import { WizardPanel, Next } from './../panel/index';
 
 const _t = {
   welcome: 'Welcome to Master Wallet',
-  start: 'Start'
+  start: 'Start',
+  ourMission: 'Our mission is to provide you full control over all of your crypto assets',
+  pleaseFollowTheSteps: 'Please follow the steps for initializing this wallet software'
 };
 
-export const WelcomeComponent = () => (
-  <WizardPanel title={_t.welcome}>
-    <Next title={_t.start} to={InstallationMenu[1]} />
+export const WelcomeComponent = () => {
+  const menu = InstallationMenu;
+  const step = 0;
 
-    <div style={{ marginTop: 30, marginBottom: 30, textAlign: 'center' }}>
-      <img style={{ width: '50%', height: 'auto' }} src='/media/temp-wallet.png' alt='' />
-    </div>
-    <FromFile name="welcome.html" />
-    
-    <Steps {...{ step: 0, menu: InstallationMenu }} />
-  </WizardPanel>
-);
+  return (
+    <WizardPanel title={_t.welcome}>
+      <Next title={_t.start} to={menu[step + 1]} />
+
+      <div style={{ marginTop: 30, marginBottom: 30, textAlign: 'center' }}>
+        <img style={{ width: 150, height: 163 }} src='/media/temp-wallet.png' alt='' />
+      </div>
+      <p style="text-align: center; font-weight: bold;">
+        {_t.ourMission}
+      </p>
+      <p style="text-align: center; margin-top: 30px">
+        {_t.pleaseFollowTheSteps}
+      </p>
+
+      <Steps {...{ step, menu }} />
+    </WizardPanel>
+  );
+};
