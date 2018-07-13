26a55cf16efbcf420aeb3e3f34b79338522bfe83
wcrbrm <viclopata@gmail.com>
2018-07-13 13:08:26 +0300

    watch wallet: address validation

--- src/components/add/watch/WatchWalletAddressComponent.js
+++ src/components/add/watch/WatchWalletAddressComponent.js
@@ -17,10 +17,10 @@ const _t = {
 
 const section = 'watch';
 export class WatchWalletAddressComponent extends React.Component {
-  state = { value: '' };
+  state = { address: '' };
 
   onChange = (v) => {
-    this.setState({ value: v })
+    this.setState({ address: v })
   };
 
   render() {
@@ -28,20 +28,22 @@ export class WatchWalletAddressComponent extends React.Component {
     const { network, testnet, selectedNetwork } = add[section];
     const menu = WatchMenu(network, testnet);
     const step = findWizardStep(menu, '/wallet');
-    const { value } = this.state;
+    const { address } = this.state;
+
+    const canContinue = !!address;
     return (
       <WizardPanel title={_t.walletAddress}>
-        <Next to={menu[step + 1]} title={_t.continue}/>
+        <Next to={menu[step + 1]} disabled={!canContinue} title={_t.continue}/>
         <Prev to={menu[step - 1]} title={_t.back}/>
         <div style={{ margin: '20px auto'}}>
           <NetworkIcon {...selectedNetwork} title={network}  style={{ margin: 20 }}/>
 
           <p style={{ textAlign: 'center', margin: 0 }}>{_t.pleaseProvide}</p>
-          <TextInput {...{value, onChange: this.onChange, autofocus: true}} />
-          {value ? (
+          <TextInput {...{value: address, onChange: this.onChange, autofocus: true}} />
+          {address ? (
             <div style={{ textAlign: 'center', marginTop: 20}}>
               <p style={{ textAlign: 'center', marginBottom: 0 }}>{_t.verifyTheImage}</p>
-              <JDentIcon size={150} value={value} style={{ margin: '0px auto', background: 'white' }} />
+              <JDentIcon size={150} value={address} style={{ margin: '0px auto', background: 'white' }} />
             </div>
           ) : false}
         </div>
