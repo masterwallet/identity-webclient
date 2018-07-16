71ede8bfba737628dced788bfcf11ff530d07957
wcrbrm <viclopata@gmail.com>
2018-07-16 15:56:49 +0300

    displaying app status

---
+++ src/components/panel/ErrorBox.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const _t = {
+  serverIsNotAvailable: 'Identity/Wallet backend is not available'
+};
+
+export const ErrorBox = styled.div`
+  background: #ffcccc;
+  opacity: 0.75;
+  position: absolute;
+  top: 10px;
+  left: 0px;
+  width: 100%;
+
+  padding-top: 10px;
+  padding-bottom: 10px;
+  font-size: 14px;
+  font-weight: bold;
+  color: darkred;
+  text-align: center;
+
+  border-bottom: darkred;
+  box-shadow: 0px 2px 10px darkred;
+
+`;
+
+export const ErrorStatus = ({ message }) => {
+  if (typeof message === 'string') {
+    if (message === 'TypeError: Failed to fetch') {
+      return (<ErrorBox>{_t.serverIsNotAvailable}</ErrorBox>);
+    }
+    return (<ErrorBox>{message}</ErrorBox>);
+  }
+  if (typeof message === 'object') {
+    if (message.message) return (<ErrorBox>{message.message}</ErrorBox>);
+    if (message.reason) return (<ErrorBox>{message.reason}</ErrorBox>);
+  }
+  return false;
+};
+
+export default {
+  ErrorBox,
+  ErrorStatus
+};
--- src/containers/App.js
+++ src/containers/App.js
@@ -31,67 +31,68 @@ const App = () => (
   <div className="App">
     <ReduxToastr {...toastr} progressBar />
     {process.env.REACT_APP_MOCK ? <MockMenu /> : false}
-    <AppStatus />
-    <Switch>
-      {/* Installation */}
-      <Route exact path='/welcome' component={Welcome} />
-      <Route exact path='/terms' component={Terms} />
-      <Route exact path='/privacy' component={Privacy} />
-      <Route exact path='/storage' component={SelectStorage} />
-      <Route exact path='/shake' component={Shake} />
-      <Route path='/seed/:page' component={Seed} />
-      <Route exact path='/confirm/seed' component={ConfirmSeed} />
-      <Route exact path='/pin' component={Pin} />
-      <Route exact path='/confirm/pin' component={ConfirmPin} />
-      <Route exact path='/setup/complete' component={Complete} />
+    <AppStatus>
+      <Switch>
+        {/* Installation */}
+        <Route exact path='/welcome' component={Welcome} />
+        <Route exact path='/terms' component={Terms} />
+        <Route exact path='/privacy' component={Privacy} />
+        <Route exact path='/storage' component={SelectStorage} />
+        <Route exact path='/shake' component={Shake} />
+        <Route path='/seed/:page' component={Seed} />
+        <Route exact path='/confirm/seed' component={ConfirmSeed} />
+        <Route exact path='/pin' component={Pin} />
+        <Route exact path='/confirm/pin' component={ConfirmPin} />
+        <Route exact path='/setup/complete' component={Complete} />
 
-      {/* Addition of Wallets / Exchange Accounts */}
-      <Route exact path='/add' component={CreateMenu} />
-      {/* Addition of Wallet to Manage */}
-      <Route exact path='/create' component={CreateWalletNetwork} />
-      <Route path='/create/url' component={CreateWalletNetworkUrl} />
-      <Route path='/create/name' component={CreateWalletName} />
-      <Route path='/create/terms' component={CreateWalletTerms} />
-      <Route path='/create/wallet' component={CreateWalletInput} />
-      <Route path='/create/paper' component={CreateWalletPaper} />
-      {/* Addition of Wallet to Watch */}
-      <Route exact path='/watch' component={WatchWalletNetwork} />
-      <Route path='/watch/url' component={WatchWalletNetworkUrl} />
-      <Route path='/watch/name' component={WatchWalletName} />
-      <Route path='/watch/wallet' component={WatchWalletAddress} />
-      <Route path='/watch/complete' component={WatchWalletComplete} />
+        {/* Addition of Wallets / Exchange Accounts */}
+        <Route exact path='/add' component={CreateMenu} />
+        {/* Addition of Wallet to Manage */}
+        <Route exact path='/create' component={CreateWalletNetwork} />
+        <Route path='/create/url' component={CreateWalletNetworkUrl} />
+        <Route path='/create/name' component={CreateWalletName} />
+        <Route path='/create/terms' component={CreateWalletTerms} />
+        <Route path='/create/wallet' component={CreateWalletInput} />
+        <Route path='/create/paper' component={CreateWalletPaper} />
+        {/* Addition of Wallet to Watch */}
+        <Route exact path='/watch' component={WatchWalletNetwork} />
+        <Route path='/watch/url' component={WatchWalletNetworkUrl} />
+        <Route path='/watch/name' component={WatchWalletName} />
+        <Route path='/watch/wallet' component={WatchWalletAddress} />
+        <Route path='/watch/complete' component={WatchWalletComplete} />
 
-      {/* Addition of Wallet by Importing */}
-      <Route exact path='/import' component={ImportWalletNetwork} />
-      <Route path='/import/url' component={ImportWalletNetworkUrl} />
-      <Route path='/import/name' component={ImportWalletName} />
-      <Route path='/import/terms' component={ImportWalletTerms} />
-      <Route path='/import/wallet' component={ImportWalletInput} />
-      <Route path='/import/complete' component={ImportWalletComplete} />
+        {/* Addition of Wallet by Importing */}
+        <Route exact path='/import' component={ImportWalletNetwork} />
+        <Route path='/import/url' component={ImportWalletNetworkUrl} />
+        <Route path='/import/name' component={ImportWalletName} />
+        <Route path='/import/terms' component={ImportWalletTerms} />
+        <Route path='/import/wallet' component={ImportWalletInput} />
+        <Route path='/import/complete' component={ImportWalletComplete} />
 
-      {/* Addition of Exchange Account to Watch */}
-      <Route exact path='/exchange' component={ExchangeSelect} />
-      <Route path='/exchange/name' component={ExchangeName} />
-      <Route path='/exchange/account' component={ExchangeInput} />
-      <Route path='/exchange/complete' component={ExchangeComplete} />
+        {/* Addition of Exchange Account to Watch */}
+        <Route exact path='/exchange' component={ExchangeSelect} />
+        <Route path='/exchange/name' component={ExchangeName} />
+        <Route path='/exchange/account' component={ExchangeInput} />
+        <Route path='/exchange/complete' component={ExchangeComplete} />
 
-      {/* Wallet Managmeent */}
-      <Route path='/wallets/:walletId/balance' component={WalletBalance} />
-      <Route path='/wallets/:walletId/receive' component={WalletReceive} />
-      <Route path='/wallets/:walletId/send'    component={WalletSend} />
-      <Route path='/wallets/:walletId/vote'    component={WalletVote} />
-      <Route path='/wallets/:walletId/account'    component={WalletAccount} />
+        {/* Wallet Managmeent */}
+        <Route path='/wallets/:walletId/balance' component={WalletBalance} />
+        <Route path='/wallets/:walletId/receive' component={WalletReceive} />
+        <Route path='/wallets/:walletId/send'    component={WalletSend} />
+        <Route path='/wallets/:walletId/vote'    component={WalletVote} />
+        <Route path='/wallets/:walletId/account'    component={WalletAccount} />
 
-      {/* Network */}
-      <Route path='/networks/:network'                     component={NetworkStatus} />
-      <Route path='/wallets/:walletId/transactions/:txHash' component={NetworkTransaction} />
+        {/* Network */}
+        <Route path='/networks/:network'                     component={NetworkStatus} />
+        <Route path='/wallets/:walletId/transactions/:txHash' component={NetworkTransaction} />
 
-      <Route exact path='/wallets' component={AssetsOverview} />
-      <Route exact path='/assets' component={AssetsCombined} />
-      <Route exact path='/unlock' component={Unlock} />
-      <Route exact path='/settings' component={Settings} />
-      <Route exact path='/' component={Home} />
-     </Switch>
+        <Route exact path='/wallets' component={AssetsOverview} />
+        <Route exact path='/assets' component={AssetsCombined} />
+        <Route exact path='/unlock' component={Unlock} />
+        <Route exact path='/settings' component={Settings} />
+        <Route exact path='/' component={Home} />
+       </Switch>
+    </AppStatus>
   </div>
 );
 
