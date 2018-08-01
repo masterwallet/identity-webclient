15af5b4a2fa83c40a9e5d22cb93f755a2ea484ae
wcrbrm <viclopata@gmail.com>
2018-08-01 11:58:00 +0300

    wallet receive - to download and reflect real data

--- src/components/wallet/WalletReceiveComponent.js
+++ src/components/wallet/WalletReceiveComponent.js
@@ -6,27 +6,44 @@ import { JDentIcon } from './../jdenticon/index';
 import QRCode from 'qrcode.react';
 
 const _t = {
-  address: 'Public Address',
+  address: 'Wallet Address',
   receive: 'Address To Send',
-  imageForVerification: 'Image For Verification'
+  imageForVerification: 'Image For Verification',
+  pleaseSelectAccount: 'Please Select Account'
 };
 
-export const WalletReceiveComponent = ({ wallet }) => {
-  const { id, address, network, name, icon } = wallet;
-  return (
-    <WalletPanel {...{id, address, name, network, icon}} back={true}>
-      <h3 style={{ fontSize: 18, textAlign: 'center', color: '#8760f6' }}>{_t.receive}</h3>
-      <div style={{ margin: '0px auto', width: 180 }}>
-        <QRCode value={address} style={{ width: 180, height: 180 }} />
-      </div>
-      <div style={{ textAlign: 'center', fontSize: 12, fontWeight: 'bold', margin: '20px 0px' }}>
-        {_t.address}:<br />
-        {address}
-      </div>
-      <div style={{ textAlign: 'center', fontSize: 12, color: '#888' }}>{_t.imageForVerification}</div>
-      <div style={{ margin: '0px auto', width: 100,  background: '#fff' }}>
-        <JDentIcon value={address} size={100} />
-      </div>
-    </WalletPanel>
-  );
-};
+export class WalletReceiveComponent extends React.Component {
+
+  componentWillMount() {
+    const id = this.props.match.params.walletId;
+    this.props.onInit({id});
+  }
+
+  render() {
+    const { wallet } = this.props;
+    const { object, isLoading, error } = wallet;
+    const { id, address, network, testnet, name, icon } = object;
+    return (
+      <WalletPanel {...object} back={true}>
+        {address ? (
+          <div>
+            <h3 style={{ fontSize: 18, textAlign: 'center', color: '#8760f6' }}>{_t.receive}</h3>
+            <div style={{ margin: '0px auto', width: 180 }}>
+              <QRCode value={address} style={{ width: 180, height: 180 }}/>
+            </div>
+            <div style={{ textAlign: 'center', fontSize: 12, fontWeight: 'bold', margin: '20px 0px' }}>
+              {_t.address}:<br />
+              {address}
+            </div>
+            <div style={{ textAlign: 'center', fontSize: 12, color: '#888' }}>{_t.imageForVerification}</div>
+            <div style={{ margin: '0px auto', width: 100,  background: '#fff' }}>
+              <JDentIcon value={address} size={100}/>
+            </div>
+          </div>
+        ) : (
+          <h3 style={{ fontSize: 18, textAlign: 'center', color: '#8760f6' }}>{_t.pleaseSelectAccount}</h3>
+        )}
+      </WalletPanel>
+    );
+  }
+}
--- src/containers/wallet/WalletReceive.js
+++ src/containers/wallet/WalletReceive.js
@@ -1,8 +1,13 @@
 import { connect } from 'react-redux';
 import { WalletReceiveComponent } from './../../components/wallet';
+import { dispatchWalletDetails } from './../../services/WalletStatus';
 
 const mapStateToProps = state => state;
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = dispatch => ({
+  onInit: ({ id }) => {
+    dispatchWalletDetails(id, dispatch);
+  }
+});
 
 export const WalletReceive = connect(mapStateToProps, mapDispatchToProps)(WalletReceiveComponent);
 export default { WalletReceive };
