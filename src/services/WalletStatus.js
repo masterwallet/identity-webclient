c44167b6541a23073060dd6d082ec8985a494951
wcrbrm <viclopata@gmail.com>
2018-08-09 12:06:54 +0300

    wallet details and list: reducing server error properly

--- src/services/WalletStatus.js
+++ src/services/WalletStatus.js
@@ -8,12 +8,15 @@ export const dispatchWalletsStatus = (dispatch) => {
   dispatch({ type: 'WALLETS_LIST_REQUEST' });
   fetchJson('/api/wallets')
     .then(response => {
-      dispatch({ type: 'WALLETS_LIST_RECEIVED', payload: response.data.wallets });
-      // console.info('WALLETS=', response.data.wallets);
-      // const queue = response.data.wallets;
+      if (response.error) {
+        dispatch({type: 'WALLETS_LIST_ERROR', payload: response.error });
+      } else {
+        dispatch({type: 'WALLETS_LIST_RECEIVED', payload: response.data.wallets});
+      }
     }).catch(err => {
       console.warn(err.toString());
       dispatch({ type: 'WALLETS_LIST_ERROR', payload: err.toString() });
+      toastr.error(err.toString());
     });
 };
 
@@ -21,8 +24,11 @@ export const dispatchWalletDetails = (id, dispatch) => {
   dispatch({ type: 'WALLET_DETAILS_REQUEST' });
   fetchJson(`/api/wallets/${id}`)
     .then(response => {
-      dispatch({ type: 'WALLET_DETAILS_RECEIVED', payload: response.data });
-      console.warn('response= ', response.data);
+      if (response.error) {
+        dispatch({ type: 'WALLET_DETAILS_ERROR', payload: response.error });
+      } else {
+        dispatch({type: 'WALLET_DETAILS_RECEIVED', payload: response.data});
+      }
     }).catch(err => {
       console.error(err.toString());
       dispatch({ type: 'WALLET_DETAILS_ERROR', payload: err.toString() });
