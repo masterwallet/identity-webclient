d35b6db63d72efc4e22699345fb8167f4763fae2
wcrbrm <viclopata@gmail.com>
2018-08-11 23:13:50 +0300

    ExchangeSelector - get rid of internal state

--- src/components/controls/ExchangeSelector.js
+++ src/components/controls/ExchangeSelector.js
@@ -1,40 +1,24 @@
 import React from 'react';
-import { fetchJson } from './../../services/ApiRequest';
 import { ErrorBox } from './../../components/panel/ErrorBox';
 import Loader from './Loader';
 import RadioButtonGroup from './RadioButtonGroup';
 
-export class ExchangeSelector extends React.Component {
-  state = {
-    isLoading: true,
-    error: '',
-    list: []
-  }
-  componentWillMount() {
-    this.setState({ isLoading: true, error: '', list: [] });
-    fetchJson('/api/exchanges').then(data => {
-      this.setState({ isLoading: false, error: '', list: data.data.exchanges });
-    }).catch(e => {
-      this.setState({ isLoading: false, error: e, list: [] });
-    });
-  }
-  render() {
-    const { isLoading, error, list } = this.state;
-    if (isLoading) return (<Loader />);
-    if (error) return <ErrorBox>{error.toString()}</ErrorBox>;
-    const { value, onChange } = this.props;
-    const options = list.map(n => ({
-      value: n.value,
-      shiftTop: 10,
-      children: [(
-        <div key={n.value} style={{ display: 'flex' }}>
-          <div style={{ lineHeight: '40px', width: 30 }}>
-            <img src={n.icon} alt='' style={{ width: 'auto', height: 25 }} />
-          </div>
-          <div style={{ lineHeight: '40px', color: '#735cbe' }}>{n.name}</div>
+export const ExchangeSelector = ({ exchangesConfig, value, onChange }) => {
+  const { isLoading, error, data } = exchangesConfig;
+  if (isLoading) return (<Loader />);
+  if (error) return <ErrorBox style={{ top: 50 }}>{error.toString()}</ErrorBox>;
+  
+  const options = data.map(n => ({
+    value: n.value,
+    shiftTop: 10,
+    children: [(
+      <div key={n.value} style={{ display: 'flex' }}>
+        <div style={{ lineHeight: '40px', width: 30 }}>
+          <img src={n.icon} alt='' style={{ width: 'auto', height: 25 }} />
         </div>
-      )]
-    }));
-    return (<RadioButtonGroup {...{value, options, onChange}} />);
-  }
-}
+        <div style={{ lineHeight: '40px', color: '#735cbe' }}>{n.name}</div>
+      </div>
+    )]
+  }));
+  return (<RadioButtonGroup {...{value, options, onChange}} />);
+};
