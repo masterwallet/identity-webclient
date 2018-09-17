b5cf561e6c4ef1dae12fb87867146f9810042de5
wcrbrm <viclopata@gmail.com>
2018-09-17 18:36:54 +0300

    home page: proper redirection on load

--- src/containers/pages/Home.js
+++ src/containers/pages/Home.js
@@ -1,20 +1,32 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { dispatchWalletsStatus } from './../../services/WalletStatus';
 
 const mapStateToProps = state => state;
-const mapDispatchToProps = dispatch => ({});
+const mapDispatchToProps = dispatch => ({
+  onInit: (props) => {
+    dispatchWalletsStatus(dispatch, props);
+  }
+});
 
-export const HomeScreen = ({ setup, assets }) => {
-  const { isFirstRun, serverStatus } = setup;
-  const { isLoading } = serverStatus;
-  if (isLoading) return false;
-  const to = isFirstRun ? '/welcome' : (
+export class HomeScreen extends React.Component {
+  componentWillMount() {
+    this.props.onInit(this.props);
+  }
+  render() {
+    const { setup, assets } = this.props;
+    const { isFirstRun, serverStatus } = setup;
+    const { isLoading } = serverStatus;
+    if (isLoading) return false;
+    const { status } = assets;
+    if (status && status.isLoading) return false;
+    const to = isFirstRun ? '/welcome' : (
       assets.wallets.length ? '/wallets' : '/add'
-  );
-  return (<Redirect to={to} />)
-};
-
+    );
+    return (<Redirect to={to} />)
+  }
+}
 
 export const Home = connect(mapStateToProps, mapDispatchToProps)(HomeScreen);
 export default { Home };
