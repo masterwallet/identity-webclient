0dcfa50dbbce134d2b7c7d58638a029fa90b9ccb
Alex Tok <aalextok@gmail.com>
2018-11-13 11:48:20 +0200

    Copy to clipboard in Electron

--- src/components/controls/Address.js
+++ src/components/controls/Address.js
@@ -2,15 +2,25 @@ import React from 'react';
 import styled from 'styled-components';
 
 export const copyToClipboard = str => {
-  const el = document.createElement('textarea');
-  el.value = str;
-  el.setAttribute('readonly', '');
-  el.style.position = 'absolute';
-  el.style.left = '-9999px';
-  document.body.appendChild(el);
-  el.select();
-  document.execCommand('copy');
-  document.body.removeChild(el);
+  let clipboard = null;
+  try {
+    clipboard = window.require('electron').clipboard;
+  } catch (e) {}
+
+  if (!clipboard) {
+    const el = document.createElement('textarea');
+    el.value = str;
+    el.setAttribute('readonly', '');
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+  } else {
+    clipboard.writeText(str);
+  }
+  
 };
 
 const AddressContainer = styled.div`
